d6de40860d76bd27d277de6e0b46444aeaff6008
Chopper <doc.chopperx@gmail.com>
2025-03-17 12:09:53 +0300

    add Stack impl

---
+++ stack/simple_stack/stack.go
@@ -0,0 +1,40 @@
+package simplestack
+
+type node struct {
+    data int
+    next *node
+}
+
+type Stack struct {
+    top *node
+}
+
+func NewStack() *Stack {
+    return &Stack {
+        top: nil,
+    }
+}
+
+func (s *Stack) Top() int {
+    return s.top.data
+}
+
+func (s *Stack) IsEmpty() bool {
+    return s.top == nil
+}
+
+func (s *Stack) Push(v int) {
+    n := &node {
+        data: v,
+        next: s.top,
+    }
+
+    s.top = n
+}
+
+func (s *Stack) Pop() *node {
+
+    ret := s.top
+    s.top = s.top.next
+    return ret
+}
