037176ad6bd51d19810b354916dffb22f5ce4c9f
Chopper <doc.chopperx@gmail.com>
2025-03-18 23:18:18 +0300

    add Stack on array impl

---
+++ stack/array_stack/stack.go
@@ -0,0 +1,43 @@
+package arraystack
+
+import "errors"
+
+var ErrStackUnderflow = errors.New("stack underflow")
+
+type Stack struct {
+    top uint
+    a   []int
+}
+
+func NewStack() *Stack {
+    return &Stack {
+        top:    0,
+        a:      make([]int, 0),
+    }
+}
+
+func (s *Stack) IsEmpty() bool {
+    return s.top == 0
+}
+
+func (s *Stack) Top() int {
+    return s.a[s.top]
+}
+ 
+func (s *Stack) Push(v int) {
+    s.a = append(s.a, v)
+    s.top++
+}
+
+func (s *Stack) Pop() (int, error) {
+
+    if s.IsEmpty() {
+        return -1, ErrStackUnderflow 
+    }
+
+    s.top--
+
+    return s.a[s.top + 1], nil
+}
+
+
